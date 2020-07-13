1cbb0aa49001550307098498320126483b957187
mariatheresa21 <37778719+mariatheresa21@users.noreply.github.com>
2020-07-12 19:25:42 -0500

    Add files via upload

---
+++ CommandPrompt.java
@@ -0,0 +1,14 @@
+
+public class AssignmentQ1 {
+
+
+	public static void main(String[] args)
+	{
+		// Print to the console
+	System.out.println("Bill Gates");
+	System.out.println("Bgates@microsoft.com");
+	System.out.println("Biological Sciences");
+	System.out.println("Department of Human Science");
+	}
+
+}
